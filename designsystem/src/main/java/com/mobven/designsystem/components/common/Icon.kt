a45104a308f898336d8ec405d821f1f244747e18
tunahanbozkurt <t.bozkurt5775@gmail.com>
2023-08-17 00:35:10 +0300

    code cleanup

--- designsystem/src/main/java/com/mobven/designsystem/components/common/Icon.kt
+++ designsystem/src/main/java/com/mobven/designsystem/components/common/Icon.kt
@@ -1,6 +1,7 @@
 package com.mobven.designsystem.components.common
 
 import androidx.annotation.DrawableRes
+import androidx.compose.material3.Icon
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -13,7 +14,7 @@ fun YummyIcon(
     modifier: Modifier = Modifier,
     tint: Color = LocalContentColor.current
 ) {
-    androidx.compose.material3.Icon(
+    Icon(
         painter = painterResource(id = painterRes),
         contentDescription = null,
         tint = tint,
