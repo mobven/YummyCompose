50a0659d7c1e9125764e67743ee3893641320b4e
Furkan Kantar <furkan.kantar@mobven.com>
2023-10-21 00:29:20 +0300

    Otp dialog added.

--- catalog/src/main/java/com/mobven/yummy/catalog/ui/Catalog.kt
+++ catalog/src/main/java/com/mobven/yummy/catalog/ui/Catalog.kt
@@ -3,7 +3,6 @@ package com.mobven.yummy.catalog.ui
 import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.add
 import androidx.compose.foundation.layout.asPaddingValues
@@ -16,42 +15,39 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import com.mobven.components.R
 import com.mobven.designsystem.components.bottomnavbar.YummyBottomNavBar
-import com.mobven.designsystem.components.buttons.CheckoutYummyButton
 import com.mobven.designsystem.components.buttons.CheckoutYummyButtonPreview
 import com.mobven.designsystem.components.buttons.CircularCounterButton
 import com.mobven.designsystem.components.buttons.CounterButton
 import com.mobven.designsystem.components.buttons.CounterButtonPreview
 import com.mobven.designsystem.components.buttons.CounterButtonType
-import com.mobven.designsystem.components.buttons.DetailedYummyButton
 import com.mobven.designsystem.components.buttons.DetailedYummyButtonPreview
 import com.mobven.designsystem.components.buttons.DisabledYummyButtonPreview
 import com.mobven.designsystem.components.buttons.EnabledYummyButtonPreview
-import com.mobven.designsystem.components.buttons.LikeButton
 import com.mobven.designsystem.components.buttons.PreviewLikeButton
-import com.mobven.designsystem.components.buttons.SocialMediaButton
 import com.mobven.designsystem.components.buttons.SocialMediaButtonPreview
-import com.mobven.designsystem.components.buttons.TinyCounterButtonPreview
 import com.mobven.designsystem.components.buttons.YummyButton
-import com.mobven.designsystem.components.buttons.YummyButtonContent
 import com.mobven.designsystem.components.buttons.YummySegmentedButtonPreview
 import com.mobven.designsystem.components.cards.PagerCardPreview
 import com.mobven.designsystem.components.common.CardOnePreview
-import com.mobven.designsystem.components.common.CardTwo
+import com.mobven.designsystem.components.common.OtpSuccessDialog
 import com.mobven.designsystem.components.common.SmallCardSix
 import com.mobven.designsystem.components.common.YummyBigCardPreview
 import com.mobven.designsystem.components.common.YummyCardTwoPreview
-import com.mobven.designsystem.theme.additionalDark
 
 @Composable
 fun Catalog() {
 
     val context = LocalContext.current
+    var isShowingOtpDialog by remember { mutableStateOf(false) }
 
     MaterialTheme {
         Surface {
@@ -167,7 +163,34 @@ fun Catalog() {
                         )
                     }
                 }
+
+                item { Text("Dialogs", Modifier.padding(top = 16.dp)) }
+                item {
+                    Column(verticalArrangement = Arrangement.spacedBy(16.dp)) {
+                        YummyButton(
+                            "Show Otp Dialog",
+                            modifier = Modifier.fillMaxWidth(),
+                            onClick = {
+                                isShowingOtpDialog = true
+                            })
+
+                    }
+                }
             }
+        }
+        if (isShowingOtpDialog) {
+            OtpSuccessDialog(phoneNumber = "05344444444",
+                onDismiss = {
+                    isShowingOtpDialog = false
+                },
+                onConfirm = {
+                    Toast.makeText(
+                        context,
+                        "OtpSuccessDialog",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    isShowingOtpDialog = false
+                })
         }
     }
 }
