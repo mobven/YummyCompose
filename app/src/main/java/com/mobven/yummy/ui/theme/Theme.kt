15989fc62a8a49e4c7cd23c1b001132401d73ad6
tunahanbozkurt <tunahan.bozkurt@mobven.com>
2023-11-01 21:39:16 +0300

    status bar icons color fixed

--- app/src/main/java/com/mobven/yummy/ui/theme/Theme.kt
+++ app/src/main/java/com/mobven/yummy/ui/theme/Theme.kt
@@ -61,7 +61,7 @@ fun YummyComposeTheme(
         SideEffect {
             val window = (view.context as Activity).window
             window.statusBarColor = Color.TRANSPARENT
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = true
         }
     }
 
