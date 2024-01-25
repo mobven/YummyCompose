a34f6e8177d0f32b477db503496b11ca4021ecde
yunusbedir <yunus.bedir@mobven.com>
2024-01-25 14:41:53 +0300

    [FIX] code reviews

--- app/src/main/java/com/mobven/yummy/ui/screens/option/OptionScreen.kt
+++ app/src/main/java/com/mobven/yummy/ui/screens/option/OptionScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -312,7 +313,8 @@ fun CounterTextRow(
                     CounterButton(
                         type = type.counterButtonType,
                         counterValue = counterValue,
-                        onCounterChanged = onCounterChanged
+                        onCounterChanged = onCounterChanged,
+                        modifier = Modifier.size(width = 96.dp, height = 32.dp)
                     )
                 }
             }
